d63caba89648de09cf48f55b5aa519ee5ca8185f
Dmitry <lbvf50.mobile@gmail.com>
2018-07-10 23:09:03 +0700

    prepare function for get nuber

--- tmp.rb
+++ tmp.rb
@@ -2,10 +2,12 @@ p 'alias x="time ruby tmp.rb"'
 require "colorize"
 require "date"
 
-date = DateTime.now
-time_for_regex = date.strftime("%d\\.%m\\.%Y") # ("/%H:%M %d\\.%m\\.%Y/")
-p time_for_regex
-regex = Regexp.new time_for_regex
-content = `cat tests/full`
-p arr = content.split("\n").select{|x| /squates/ === x}.select{|x| regex === x}.reverse
-p arr[0].match(/\s*(\d{2,})\s*$/)[1].to_i + 1
+def get_num(file_name)
+    time_for_regex = DateTime.now.strftime("%d\\.%m\\.%Y")
+    regex = Regexp.new time_for_regex
+    content = `cat #{file_name}`
+    arr = content.split("\n").select{|x| /squates/ === x}.select{|x| regex === x}.reverse
+    arr.empty? ? '01' : format('%02d',arr[0].match(/\s*(\d{2,})\s*$/)[1].to_i + 1)
+end
+
+p get_num('tests/full')
